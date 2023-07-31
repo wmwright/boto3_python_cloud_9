f5ba5367d4eb4fc75d6b0f41f04a4577bb35e80b
William Wright <william.a.wrightii@gmail.com>
2023-07-31 10:56:39 +0000

    create ec2

---
+++ EC2/boto3_work_create_ec2_2.py
@@ -0,0 +1,20 @@
+import boto3
+
+ami_id = ""
+key_pair_name = ""
+security_group_id = ""
+
+ec2 = boto3.client('ec2')
+
+response = ec2.run_instances(
+    ImageId=ami_id,
+    InstanceType='t2.micro',
+    KeyName=key_pair_name,
+    MaxCount=1,
+    MinCount=1,
+    SecurityGroupIds=[
+        security_group_id
+    ]
+)
+
+print(response["Insatnces"][0]["InstanceId"])
